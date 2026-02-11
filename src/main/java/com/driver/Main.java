f3e7706263e5e2f8ceddbd7f5b0e0f7a5301cfee
Varun Sangewar <114732914+varunsangewar03@users.noreply.github.com>
2026-02-11 22:34:23 +0530

    Add main method to execute class methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        A obj1 = new B();
+        System.out.println(obj1.meth()); 
+
+        B obj2 = new B();
+        System.out.println(obj2.meth());
+    }
 }
